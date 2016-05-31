7739a6594cd1660309ec3c0ac2fa46e47350a9e4
MJKAEM <Martinokuan@gmail.com>
2016-05-30 21:45:13 -0700

    Initial commit.

--- README.md
+++
@@ -1,1 +0,0 @@
-# PathFindingPrototype
---
+++ src/mjkuan/pathfinding/Main.java
@@ -0,0 +1,10 @@
+package mjkuan.pathfinding;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+	public static void main(String[] argv)
+	{
+		PApplet.main(Main.class.getName());
+	}
+}
