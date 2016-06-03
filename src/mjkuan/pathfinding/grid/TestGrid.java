e64928cd97010eb07b442a11650fd12a5c5d2c74
MJKAEM <Martinokuan@gmail.com>
2016-06-02 23:58:53 -0700

    Change the test grid to use the correct size.

--- src/mjkuan/pathfinding/grid/TestGrid.java
+++ src/mjkuan/pathfinding/grid/TestGrid.java
@@ -1,16 +1,16 @@
 package mjkuan.pathfinding.grid;
 
-public class TestGrid extends Grid{
+public class TestGrid extends Grid {
 	public TestGrid()
 	{
-		super(10, 10);
+		super(10, 7);
 		GridPosition playerPosition = new GridPosition(0, 0);
 		add(new Player(playerPosition));
-		
+
 		GridPosition rockPosition001 = new GridPosition(1, 1);
 		add(new Rock(rockPosition001));
-		
-		GridPosition waterPosition001 = new GridPosition(9, 9);
+
+		GridPosition waterPosition001 = new GridPosition(9, 6);
 		add(new Water(waterPosition001));
 	}
 }
