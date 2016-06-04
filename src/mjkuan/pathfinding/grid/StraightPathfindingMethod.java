883b30393224808d0b7bdc55bfb7e0163b3f2832
MJKAEM <Martinokuan@gmail.com>
2016-06-04 01:06:53 -0700

    Complete implementation of straight pathfinding.

--- src/mjkuan/pathfinding/grid/StraightPathfindingMethod.java
+++ src/mjkuan/pathfinding/grid/StraightPathfindingMethod.java
@@ -4,26 +4,25 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class StraightPathfindingMethod extends PathfindingMethod {
-	
+
 	public StraightPathfindingMethod(GridPosition currentPath)
 	{
 		super(currentPath);
 	}
-	
+
 	@Override
 	protected List<GridPosition> generatePath(GridPosition targetPosition)
 	{
 		List<GridPosition> pathList = new LinkedList<>();
-		pathList.add(getCurrentPosition());
-		
+
 		GridPosition currentPosition = getCurrentPosition();
-		while(currentPosition != targetPosition) {
+		while (!currentPosition.equals(targetPosition)) {
 			GridDirections facingDirection = currentPosition.getDirectionFrom(targetPosition);
 			GridPosition nextPosition = currentPosition.getAdjacentPosition(facingDirection);
 			pathList.add(nextPosition);
 			currentPosition = nextPosition;
 		}
-		
-		return null;
+
+		return pathList;
 	}
 }
