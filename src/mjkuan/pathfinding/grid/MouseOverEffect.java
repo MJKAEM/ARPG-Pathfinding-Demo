af46da34982b9090292fe786d5c5c8261047cb36
MJKAEM <Martinokuan@gmail.com>
2016-06-04 01:07:55 -0700

    Make visibility variable in MouseOverEffect public.

--- src/mjkuan/pathfinding/grid/MouseOverEffect.java
+++ src/mjkuan/pathfinding/grid/MouseOverEffect.java
@@ -3,31 +3,24 @@ package mjkuan.pathfinding.grid;
 import mjkuan.pathfinding.Global;
 
 public class MouseOverEffect {
-	private static boolean visible = false;
+	static boolean visible = true;
 	private GridPosition position;
-	
+
 	public MouseOverEffect()
 	{
 		reset();
 	}
-	
-	public static void toggleVisibility()
-	{
-		visible = !visible;
-	}
-	
-	public static boolean isVisible()
-	{
-		return visible;
-	}
-	
+
 	public void show()
 	{
-		Global.callP5().noFill();
-		Global.callP5().stroke(128, 128, 255);
-		Global.callP5().rect(position.getX() * Tile.TILE_WIDTH, position.getY() * Tile.TILE_HEIGHT, Tile.TILE_WIDTH, Tile.TILE_HEIGHT);
+		if (visible) {
+			Global.callP5().noFill();
+			Global.callP5().stroke(128, 128, 255);
+			Global.callP5().rect(position.getX() * Tile.TILE_WIDTH, position.getY() * Tile.TILE_HEIGHT, Tile.TILE_WIDTH,
+					Tile.TILE_HEIGHT);
+		}
 	}
-	
+
 	public void update()
 	{
 		if (Global.callP5().mouseX < position.getX() * Tile.TILE_WIDTH) {
@@ -36,7 +29,7 @@ public class MouseOverEffect {
 		else if (Global.callP5().mouseX > (position.getX() + 1) * Tile.TILE_WIDTH) {
 			position.move(GridDirections.EAST);
 		}
-		
+
 		if (Global.callP5().mouseY < position.getY() * Tile.TILE_HEIGHT) {
 			position.move(GridDirections.NORTH);
 		}
@@ -44,14 +37,14 @@ public class MouseOverEffect {
 			position.move(GridDirections.SOUTH);
 		}
 	}
-	
+
 	public void reset()
 	{
-		int x = (int)(Global.callP5().mouseX / Tile.TILE_WIDTH);
-		int y = (int)(Global.callP5().mouseY / Tile.TILE_HEIGHT);
+		int x = (int) (Global.callP5().mouseX / Tile.TILE_WIDTH);
+		int y = (int) (Global.callP5().mouseY / Tile.TILE_HEIGHT);
 		position = new GridPosition(x, y);
 	}
-	
+
 	public GridPosition getPosition()
 	{
 		return this.position;
